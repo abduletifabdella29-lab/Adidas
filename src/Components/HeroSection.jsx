f08438c73005ffe0fb3905f4d4dcd11cac0ca610
Abduletif Abdella <abduletifabdella29@gmail.com>
2026-08-01 18:37:43 +0300

    format HeroSection code

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,30 +1,50 @@
-import React from 'react'
-import herobg from "../Assests/Images/herobg.png"
-import adidas from "../Assests/Icons/adidas.png"
-import rightarrow from "../Assests/Icons/rightarrow.png"
+import React from "react";
+import herobg from "../Assests/Images/herobg.png";
+import adidas from "../Assests/Icons/adidas.png";
+import rightarrow from "../Assests/Icons/rightarrow.png";
 
 function HeroSection() {
-    return (
-        <>
-        <div 
-            className="mt-4 bg-cover bg-center bg-no-repeat w-full"
-            style={{ backgroundImage: `url(${herobg})` }}>
-
-            <div>
-
-            <h1 className="pt-40 pl-16 text-6xl font-bold bg-gradient-to-red from-red-500 via-black to-black bg-clip-text text-transparent">
+  return (
+    <>
+      <div
+        className="mt-4 bg-cover bg-center bg-no-repeat w-full"
+        style={{ backgroundImage: `url(${herobg})` }}
+      >
+        <div>
+          <h1 className="pt-40 pl-16 text-6xl font-bold bg-gradient-to-red from-red-500 via-black to-black bg-clip-text text-transparent">
             ADICLUB <br /> IS SHOWING OFF.
-            </h1>
+          </h1>
 
-            <p className='pl-16 pt-5 text-2xl text-white'>Exclusive releases for our members and more ways <br />to unlock those things that you love.</p>
+          <p className="pl-16 pt-5 text-2xl text-white">
+            Exclusive releases for our members and more ways <br />
+            to unlock those things that you love.
+          </p>
 
-            <p className='pl-16 pt-8'><a href="#" className='text-white font-extrabold text-3xl'><img src={rightarrow} alt="right-arrow" className='inline-block'/> SEE MORE</a></p>
+          <p className="pl-16 pt-8">
+            <a href="#" className="text-white font-extrabold text-3xl">
+              <img
+                src={rightarrow}
+                alt="right-arrow"
+                className="inline-block"
+              />{" "}
+              SEE MORE
+            </a>
+          </p>
 
-            <p className='pl-16 pt-8'><a href="#" className='text-white font-extrabold text-3xl'><img src={rightarrow} alt="right-arrow" className='inline-block'/> BUY NOW</a></p>
-            </div>
+          <p className="pl-16 pt-8">
+            <a href="#" className="text-white font-extrabold text-3xl">
+              <img
+                src={rightarrow}
+                alt="right-arrow"
+                className="inline-block"
+              />{" "}
+              BUY NOW
+            </a>
+          </p>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
