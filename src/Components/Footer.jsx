9fc1c6e745c07ec6f9addf7a80163e9014a73fa0
Abduletif Abdella <abduletifabdella29@gmail.com>
2026-08-08 09:32:08 +0300

    feat: add name to the Footer Section

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -11,69 +11,76 @@ import twitter from "../Assests/Icons/twitter.png"
 function Footer() {
     return (
     <>
-    <div className=' flex bg-cover bg-center bg-no-repeat w-full h-88' style={{backgroundColor: '#231F20'}}>
-        
-        <div className='text-white text-center pt-23 pl-16'>
-            <h1 className='text-3xl font-extrabold'>INFO</h1>
-            <div className='font-light'>
-            <a href='#'>Terms and Condition</a> <br />
-            <a href='#'>Cookies</a> <br />
-            <a href='#'>Privacy Politics</a>
-            </div>
-        </div>
-
-        <div className='text-white text-center pt-23 pl-21'>
-            <h1 className='text-3xl font-extrabold'>COLLECTIONS</h1>
-            <div className='font-light'>
-            <a href='#'>Ultraboost</a> <br />
-            <a href='#'>Confirmed</a> <br />
-            <a href='#'>NMD</a> <br />
-            <a href='#'>Originals</a>
-            </div>
-        </div>
-
-        <div className='text-white text-center pl-20'>
-            <div className='pl-10 pt-8'>
-            <img src={adidas2} alt="addidas" />
-            </div>
-
-            <h1 className='text-3xl font-extrabold pt-4'>SPORTS</h1>
-            <div className='font-light'>
-            <a href='#'>Football</a> <br />
-            <a href='#'>Baskerball</a> <br />
-            <a href='#'>Running</a> <br />
-            <a href='#'>Training</a>
-            </div>
-            <div className='flex gap-5 pt-18'>
-                <div><a href="#"><img src={amex} /></a></div>
-                <div className='pt-1'><a href="#"><img src={mastercard} /></a></div>
-                <div className='pt-2'><a href="#"><img src={visa} /></a></div>
-            </div>
-        </div>
-
-        <div className='text-white text-center pt-23 pl-21'>
-            <h1 className='text-3xl font-extrabold'>SUPPORT</h1>
-            <div className='font-light'>
-            <a href='#'>Customer</a> <br />
-            <a href='#'>F.A.Q.</a> <br />
-            <a href='#'>Shipping</a> <br />
-            <a href='#'>adiClub</a>
-            </div>
-        </div>
-
-        <div className='text-white text-center pt-23 pl-21'>
-            <h1 className='text-3xl font-extrabold'>FOLLOWS US</h1>
-            <div className='flex gap-5 pt-5'>
-                <div className=' pl-11 gap-7 flex'>
-                    <div><a href="#"><img src={facebook} /></a></div>
-                    <div className='pt-1'><a href="#"><img src={youtube} /></a></div>
+    <div className=' bg-cover bg-center bg-no-repeat w-full h-98' style={{backgroundColor: '#231F20'}}>
+        <div className='flex'>
+            <div className='text-white text-center pt-23 pl-16'>
+                <h1 className='text-3xl font-extrabold'>INFO</h1>
+                <div className='font-light'>
+                <a href='#'>Terms and Condition</a> <br />
+                <a href='#'>Cookies</a> <br />
+                <a href='#'>Privacy Politics</a>
                 </div>
             </div>
 
-            <div className=' pl-11 pt-5 gap-7 flex'>
-                <div className=''><a href="#"><img src={instagram} /></a></div>
-                <div className='pt-1'><a href="#"><img src={twitter} /></a></div>
+            <div className='text-white text-center pt-23 pl-21'>
+                <h1 className='text-3xl font-extrabold'>COLLECTIONS</h1>
+                <div className='font-light'>
+                <a href='#'>Ultraboost</a> <br />
+                <a href='#'>Confirmed</a> <br />
+                <a href='#'>NMD</a> <br />
+                <a href='#'>Originals</a>
+                </div>
             </div>
+
+            <div className='text-white text-center pl-20'>
+                <div className='pl-10 pt-8'>
+                <img src={adidas2} alt="addidas" />
+                </div>
+
+                <h1 className='text-3xl font-extrabold pt-4'>SPORTS</h1>
+                <div className='font-light'>
+                <a href='#'>Football</a> <br />
+                <a href='#'>Baskerball</a> <br />
+                <a href='#'>Running</a> <br />
+                <a href='#'>Training</a>
+                </div>
+                <div className='flex gap-5 pt-18'>
+                    <div><a href="#"><img src={amex} /></a></div>
+                    <div className='pt-1'><a href="#"><img src={mastercard} /></a></div>
+                    <div className='pt-2'><a href="#"><img src={visa} /></a></div>
+                </div>
+            </div>
+
+            <div className='text-white text-center pt-23 pl-21'>
+                <h1 className='text-3xl font-extrabold'>SUPPORT</h1>
+                <div className='font-light'>
+                <a href='#'>Customer</a> <br />
+                <a href='#'>F.A.Q.</a> <br />
+                <a href='#'>Shipping</a> <br />
+                <a href='#'>adiClub</a>
+                </div>
+            </div>
+
+            <div className='text-white text-center pt-23 pl-21'>
+                <h1 className='text-3xl font-extrabold'>FOLLOWS US</h1>
+                <div className='flex gap-5 pt-5'>
+                    <div className=' pl-11 gap-7 flex'>
+                        <div><a href="#"><img src={facebook} /></a></div>
+                        <div className='pt-1'><a href="#"><img src={youtube} /></a></div>
+                    </div>
+                </div>
+
+                <div className=' pl-11 pt-5 gap-7 flex'>
+                    <div className=''><a href="#"><img src={instagram} /></a></div>
+                    <div className='pt-1'><a href="#"><img src={twitter} /></a></div>
+                </div>
+            </div>
+        </div>
+        
+        <div>
+            <a href="https://github.com/abduletifabdella29-lab">
+                <h1 className='text-center font-bold pt-6 pl-5 text-2xl text-white'>Bulit By Abduletif Abdella</h1>
+            </a>
         </div>
     </div>
     </>
